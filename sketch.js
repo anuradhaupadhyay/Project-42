af49acd8f3755ab4d2a43ead99d3f88e79464152
anuradhaupadhyay <anuradhaupadhyay81@gmail.com>
2020-11-11 14:57:30 +0530

    Clock

--- sketch.js
+++ sketch.js
@@ -1,58 +1,50 @@
-var ho, hoAngle;
-var mi, miAngle;
-var se, seAngle;
+var hr,mn,sc,hrAngle,mnAngle,scAngle
+
 
 function setup() {
-  createCanvas(800,800);
-
-
+  createCanvas(400,400);
+  angleMode(DEGREES);
 }
 
 function draw() {
   background(0);
-
-  ho = hour();
-  mi = minute();
-  se = second();
-
-  angleMode(DEGREES);
-
-  hoAngle = map(ho, 0, 60, 0, 360);
-  miAngle = map(mi, 0, 60, 0, 360);
-  seAngle = map(se, 0, 60, 0, 360);
-
-
-
+  translate(200,200);
+  rotate(-90);  
+  hr = hour();
+  mn = minute();
+  sc = second();
+  scAngle = map(sc,0,60,0,360);
+  mnAngle = map(mn,0,60,0,360);
+  hrAngle = map(hr%12,0,12,0,360);
   push();
-  translate(400, 400);
-  strokeWeight(5);
-  stroke(0,0,255);
-  rotate(seAngle - 90);
-  line(0,0,100,0);
-  noFill();
-  arc(0,0,210,210,-seAngle,0)
-  pop();
-
-  push();
-  translate(400, 400);
-  strokeWeight(7);
+  rotate(scAngle)
   stroke(255,0,0);
-  rotate(miAngle - 90);
-  line(0,0,200,0);
-  noFill();
-  arc(0,0,410,410,-miAngle,0)
+  strokeWeight(7);
+  line(0,0,100,0);
   pop();
-
   push();
-  translate(400, 400);
-  strokeWeight(9);
+  rotate(mnAngle);
   stroke(0,255,0);
-  rotate(hoAngle - 90);
-  line(0,0,50,0);
-  noFill();
-  arc(0,0,110,110,hoAngle,0)
+  strokeWeight(7);
+  line(0,0,75,0);
   pop();
-
+  push();
+  rotate(hrAngle);
+  stroke(0,0,255);
+  strokeWeight(7);
+  line(0,0,50,0);
+  pop();
+  stroke(255,0,255);
+  point(0,0);
+  strokeWeight(10);
+  noFill();
+  stroke(255,0,0);
+  arc(0,0,300,300,0,scAngle);
+  stroke(0,255,0);
+  arc(0,0,280,280,0,mnAngle);
+  stroke(0,0,255);
+  arc(0,0,260,260,0,hrAngle);
+  
 
   drawSprites();
 }
