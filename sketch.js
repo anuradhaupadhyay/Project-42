e4c6d9366cfe40f064d0243405bd32162769eddc
anuradhaupadhyay <anuradhaupadhyay81@gmail.com>
2020-11-11 15:04:47 +0530

    Clock

--- sketch.js
+++ sketch.js
@@ -16,25 +16,28 @@ function draw() {
   scAngle = map(sc,0,60,0,360);
   mnAngle = map(mn,0,60,0,360);
   hrAngle = map(hr%12,0,12,0,360);
+
   push();
   rotate(scAngle)
   stroke(255,0,0);
   strokeWeight(7);
   line(0,0,100,0);
   pop();
+
   push();
   rotate(mnAngle);
   stroke(0,255,0);
   strokeWeight(7);
   line(0,0,75,0);
   pop();
+
   push();
   rotate(hrAngle);
   stroke(0,0,255);
   strokeWeight(7);
   line(0,0,50,0);
   pop();
-  stroke(255,0,255);
+  
   point(0,0);
   strokeWeight(10);
   noFill();
